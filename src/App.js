2d6d7dba4f7d45bd4311e6a6ff8324858271d76d
Rossprehn <32393462+Rossprehn@users.noreply.github.com>
2018-02-01 16:14:05 -0700

    list and form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Header from './Header'
+import Footer from './Footer'
+// import JobList from './JobList'
+import JobForm from './JobForm'
 
 class App extends Component {
+  state = { jobs: [] }
+
+  componentDidMount() {
+    this.getListings()
+  }
+
+  getListings = () => {
+    return fetch('./listings.json')
+      .then(response => response.json())
+      .then(jobs => {
+        console.log(jobs)
+        this.setState({ jobs })
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <main>
+          <section>
+            <ul id="job-listings">
+              {this.state.jobs.map(job => (
+                <li key={job.title}>
+                  <h4>{job.title}</h4>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <aside id="side-bar">
+            <h3>Add a Job</h3>
+            <JobForm />
+          </aside>
+        </main>
+        <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
